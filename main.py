7ac0b25f5522a8e702b0a96e99b3ef424b919517
Hernan Ferreras <hernanferreras@icloud.com>
2024-03-19 07:43:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+def input_num():
+    a = int(input("ingrese un numero no negativo para imprimir en el led: "))
+    while a < 0:
+        print("El numero debe ser mayor o igual a 0")
+        a = input_num()
+    return a
+
 led = (('***','* *','* *','* *','***')    #numero_0
     ,('  *','  *','  *','  *','  *')    #numero_1
     ,('***','  *','***','*  ','***')    #numero_2    
@@ -10,9 +17,9 @@ led = (('***','* *','* *','* *','***')    #numero_0
     ,('***','* *','***','  *','***')    #numero_9
     )
 
-num = input("ingrese un numero no negativo para imprimir en el led: ")
+num = str(input_num())
 
 for z in range(5):
     for x in num:
-        print(led[int(x)][z], end=" ")
+        print(led[int(x)][z], end=' ')
     print()
